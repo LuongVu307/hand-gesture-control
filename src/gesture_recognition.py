0aca5ea9462e62c33e7078ccda2c76a231478156
Gia Luong Vu <lgvu307@gmail.com>
2025-05-22 16:52:31 +0100

    Initial gesture recognition setup

---
+++ src/gesture_recognition.py
@@ -0,0 +1,9 @@
+def recognize_gesture(hand_landmarks):
+    """
+    Dummy example: identify if hand is open or fist based on landmarks.
+    You should improve this with actual logic or ML model.
+    """
+    # For example, check if fingers are folded or extended
+    # Return gesture name like "fist", "open_hand"
+    # For now, always return "open_hand" as placeholder
+    return "open_hand"
