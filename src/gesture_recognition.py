a991af553a36a9c949ad3da9d103c2016d7666d2
Gia Luong Vu <lgvu307@gmail.com>
2025-05-24 13:33:57 +0100

    Update gesture_recognition.py
    - Add states to the hand the fingers
    - Add method "get_gesture" to get the gesture in __main__.py
    - Implement logic of finger close/open

--- src/gesture_recognition.py
+++ src/gesture_recognition.py
@@ -8,16 +8,59 @@ class GestureDetector:
             "ring" : {},
             "pinky" : {},
         }
+        self.hand_state = None
 
     def update(self, hand_landmarks):
         if hand_landmarks != None:
             hand_landmarks = hand_landmarks.landmark
             self.hand["wrist"] = hand_landmarks[0]
             
-            counter = 1
+            multiplier = 0
             for finger in list(self.hand.keys())[1:]:
-                self.hand[finger][(counter-1)%4] = hand_landmarks[counter]
-                counter += 1
+                for counter in range(4):
+                    self.hand[finger][counter] = hand_landmarks[counter+4*multiplier+1]
+                multiplier += 1
+        
+        self.set_hand_state()
+        
+
+ 
+    def get_fingers_state(self):
+
+        finger_states = {
+            "thumb" : None,
+            "index" : None,
+            "middle" : None,
+            "ring" : None,
+            "pinky" : None,
+        }
+
+
+        for finger in list(self.hand.keys())[1:]:
+            if self.hand[finger][3].y < self.hand[finger][2].y < self.hand[finger][1].y < self.hand[finger][0].y:
+                finger_states[finger] = "fully_open"
+            elif self.hand[finger][1].y < self.hand[finger][3].y < self.hand[finger][2].y:
+                finger_states[finger] = "fully_closed"
+            elif self.hand[finger][1].y < self.hand[finger][2].y < self.hand[finger][3].y:
+                finger_states[finger] = "half_closed"
+        return finger_states
+            
+    
+    def set_hand_state(self):
+        finger_states = self.get_fingers_state()
+        print(finger_states.values())
+        
+        if len(set(finger_states.values())) and "fully_open" in finger_states.values():
+            self.hand_state = "fully_open_hand"
+        else:
+            self.hand_state = "close_hand"
+            for finger in ["index", "middle", "ring", "pinky"]:
+                if finger_states[finger] not in ["fully_closed", "half_closed"]:
+                    self.hand_state = None
+                    break
+
+            
 
     def get_gesture(self):
-        return "open_hand"
+        return self.hand_state
+        
