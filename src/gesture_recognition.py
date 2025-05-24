b19bd98a25fe8f9407de4178c027cdaa2e536de3
Gia Luong Vu <lgvu307@gmail.com>
2025-05-24 11:22:56 +0100

    Update: logic of Gesture Detector update method

--- src/gesture_recognition.py
+++ src/gesture_recognition.py
@@ -9,8 +9,13 @@ class GestureDetector:
             "pinky" : {},
         }
 
-    def update(hand_landmarks):
-        ...
+    def update(self, hand_landmarks):
+        self.hand["wrist"] = hand_landmarks[0]
+        
+        counter = 1
+        for finger in self.hand.keys()[1:]:
+            self.hand[finger][(counter-1)%4] = hand_landmarks[counter]
+            counter += 1
 
     def get_gesture():
         ...
