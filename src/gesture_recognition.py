d20ec4dd7df54bfc58b8d556be7bd3b572ad1171
Gia Luong Vu <lgvu307@gmail.com>
2025-05-24 11:12:45 +0100

    Initialize Gesture Detector as a class

--- src/gesture_recognition.py
+++ src/gesture_recognition.py
@@ -1,9 +1,9 @@
-def recognize_gesture(hand_landmarks):
-    """
-    Dummy example: identify if hand is open or fist based on landmarks.
-    You should improve this with actual logic or ML model.
-    """
-    # For example, check if fingers are folded or extended
-    # Return gesture name like "fist", "open_hand"
-    # For now, always return "open_hand" as placeholder
-    return "open_hand"
+class GestureDetector:
+    def __init__(self):
+        ...
+
+    def update(hand_landmarks):
+        ...
+
+    def get_gesture():
+        ...
