aa7202387600c4f44dd324cddc3f20f398424ae9
Gia Luong Vu <lgvu307@gmail.com>
2025-05-24 15:01:41 +0100

    Update: Intergrate check audio playing into playing/pausing audio

--- src/command_executor.py
+++ src/command_executor.py
@@ -1,21 +1,23 @@
 import pyautogui
+from .utils import is_audio_playing
 
 class CommandExecutor:
     def __init__(self):
-        self.play_state = None
+        self.play_state = is_audio_playing()
 
     def execute_command(self, command_name):
         """
         Map recognized gesture names to actions.
         Extend this as you add more gestures.
         """
-        if command_name == "fist":
-            pyautogui.press('pause')
-            self.play_state = False
-        elif command_name == "open_hand":
-            if self.play_state == False:
-                pyautogui.press('play')
+        
+        if command_name == "close_hand":
+            if self.play_state ==False:
+                pyautogui.press('playpause')
                 self.play_state = True
-
+        elif command_name == "fully_open_hand":
+            if self.play_state == True:
+                pyautogui.press('playpause')
+                self.play_state = False
         else:
             print(f"Unknown command: {command_name}")
