326697996ec7593175e2a31d838334b57cc4e899
Gia Luong Vu <lgvu307@gmail.com>
2025-05-24 11:39:17 +0100

    Fix: update logic of Gesture Detection update

--- src/gesture_recognition.py
+++ src/gesture_recognition.py
@@ -10,12 +10,14 @@ class GestureDetector:
         }
 
     def update(self, hand_landmarks):
-        self.hand["wrist"] = hand_landmarks[0]
-        
-        counter = 1
-        for finger in self.hand.keys()[1:]:
-            self.hand[finger][(counter-1)%4] = hand_landmarks[counter]
-            counter += 1
+        if hand_landmarks != None:
+            hand_landmarks = hand_landmarks.landmark
+            self.hand["wrist"] = hand_landmarks[0]
+            
+            counter = 1
+            for finger in list(self.hand.keys())[1:]:
+                self.hand[finger][(counter-1)%4] = hand_landmarks[counter]
+                counter += 1
 
-    def get_gesture():
-        ...
+    def get_gesture(self):
+        return "open_hand"
