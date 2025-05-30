2f059d2cdfa6a9d181aa1ebcef2ea654f4d6103e
Gia Luong Vu <lgvu307@gmail.com>
2025-05-30 21:01:06 +0100

    Update: integrate hand, fingers' states into gesture

--- src/gesture_recognition.py
+++ src/gesture_recognition.py
@@ -3,20 +3,30 @@ from .hand import Hand
 class GestureDetector:
     def __init__(self):
         self.hand = Hand()
-        self.maps = {
-            
+        self.map = {
+            "open"+"close"*4: "close",
+            "open"*5: "open"
         }
 
     def update(self, hand_landmarks):
         if hand_landmarks != None:
             self.hand.update(hand_landmarks[0])
 
-        print(self.hand.get_angle_palm_finger("pinky"))
                     
     def map_hand_state(self):
-        return 0
+        current_state = self.get_hand_state()
+        return self.map[current_state] if current_state in self.map else None
     
     
     def get_gesture(self):
         return self.map_hand_state()
+    
+    def get_hand_state(self):
+        states = []
+        for finger_type in ["thumb", "index", "middle", "ring", "pinky"]:
+            finger = getattr(self.hand, finger_type)
+            states.append(finger.state)
+
+        return "".join(states)
+
         
